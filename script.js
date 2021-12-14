c0c8c08a0780b931205cb9b3b31e0d028287e330
VeroniqueJakobs <93218554+VeroniqueJakobs@users.noreply.github.com>
2021-12-14 19:15:47 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+//Function how to make pizza
+const makePizza = function(){
+    console.log ("Gather the ingredients");
+    console.log ("Put the ingredients on the dough");
+    console.log ("Put the pizza in the oven(heated at 200 degrees Celsius)");
+};
+
+
+//Function how to make sushi
+const makeSushi = function(){
+    console.log ("Verzamel de ingrediënten");
+    console.log ("Spreid de rol van zeewier");
+    console.log ("Leg de ingrediënten op het zeewier");
+    console.log ("Maak een rol van het zeewier");
+    console.log ("Snij deze rol in kleine stukjes");
+};
+
+//Call on functions
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
